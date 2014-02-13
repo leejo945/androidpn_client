0a9f1ac50b897da4b8d17f5d6b32084773dd8868
leejo945 <leejo945@163.com>
2014-02-13 18:02:31 +0800

    2   13

---
+++ src/com/pn/client/widget/MyWidget.java
@@ -0,0 +1,60 @@
+package com.pn.client.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.sax.StartElementListener;
+
+public class MyWidget extends AppWidgetProvider {
+   private 	 Intent  i;
+	@Override
+	public IBinder peekService(Context myContext, Intent service) {
+		// TODO Auto-generated method stub
+		return super.peekService(myContext, service);
+	}
+
+	 
+//	public void onAppWidgetOptionsChanged(Context context,
+//			AppWidgetManager appWidgetManager, int appWidgetId,
+//			Bundle newOptions) {
+//		 
+//		super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId,
+//				newOptions);
+//	}
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		
+		 
+		super.onReceive(context, intent);
+	}
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+	 
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+	}
+
+	@Override
+	public void onDeleted(Context context, int[] appWidgetIds) {
+		 
+		super.onDeleted(context, appWidgetIds);
+	}
+
+	@Override
+	public void onEnabled(Context context) {
+		 
+		 super.onEnabled(context);
+	}
+
+	@Override
+	public void onDisabled(Context context) {
+		 
+		super.onDisabled(context);
+	}
+
+}
---
+++ src/com/pn/client/widget/WidgetService.java
@@ -0,0 +1,53 @@
+package com.pn.client.widget;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+ 
+
+import android.app.Service;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.RemoteViews;
+
+public class WidgetService extends Service {
+   private Timer timer;
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public void onCreate() {
+		 
+		 timer = new Timer();
+		TimerTask task = new TimerTask() {
+			
+			@Override
+			public void run() {
+//				 ComponentName name = new ComponentName(getPackageName(), getPackageName()+".MyWidget");
+//				RemoteViews views = new RemoteViews(getPackageName(), R.layout.example_appwidget); 
+//				int time =new Random().nextInt();
+//				views.setTextViewText(R.id.textView1,time+"");
+//				AppWidgetManager  am =	AppWidgetManager.getInstance(getApplicationContext());
+//				 
+//				am.updateAppWidget(name, views);
+//				System.out.println("----"+time);
+//				
+				
+			}
+		};
+		timer.scheduleAtFixedRate(task, 0, 1000);
+	};
+  @Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+	     System.out.println("服务关闭了");
+	     if(timer!=null){
+	    	 timer.cancel();
+	     }
+		super.onDestroy();
+	}
+}
